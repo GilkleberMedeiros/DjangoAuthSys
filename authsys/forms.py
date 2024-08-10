52eee9f82623783363f53407094d31b36a390361
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-08-10 17:56:08 -0300

    Permissões e funções auxiliares para permissões criadas, decoradores de 'permission_required' adicionados

--- authsys/forms.py
+++ authsys/forms.py
@@ -1,10 +1,15 @@
-from django.forms import Form, ModelForm
+from django.forms import Form, ModelForm, BooleanField
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 
 class UserSignUpModelForm(UserCreationForm):
+    prime_user_permission = BooleanField(
+        label="Quer ter permissão de usuário prime?",
+        required=False,
+        )
+
     class Meta:
         model = User
         fields = ("username", "email",)
--- authsys/models.py
+++ authsys/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Dummy(models.Model):
+    dummy_field = models.CharField(max_length=50, default="dummy")
+
+    class Meta:
+        app_label = "dummy"
