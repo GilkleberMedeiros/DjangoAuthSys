39f48be6e2bb1902cba25385a9869480a05bc02a
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-08-18 13:49:52 -0300

    Mixin para confirmação de emails de validação criada.

--- authsys/mixins.py
+++ authsys/mixins.py
@@ -17,7 +17,7 @@ class SendValidateEmailMixin:
         return token
     
     def store_token(self, token: str):
-        settings.VALIDATION_EMAILS_SENDED[self.user.username] = token
+        settings.VALIDATION_EMAILS_SENT[self.user.username] = token
     
     def set_context(self):
         from datetime import datetime
@@ -43,3 +43,47 @@ class SendValidateEmailMixin:
         email_str = self.render_email()
 
         user.email_user(self.email_subject, email_str, self.from_email)
+
+
+class ConfirmValidationEmailMixin:
+    confirm_failed_template = "emails/failed_confirm.html"
+    confirm_success_template = "emails/success_confirm.html"
+    __context = {}
+    extra_context = {}
+
+    def valid_email(self, user, token, datetime):
+        from datetime import datetime as dt
+        from django.template.loader import render_to_string
+        from .utils import is_permission_created_create
+        from .models import Dummy
+
+        miliseconds_passed = ((dt.now() - datetime).total_seconds() * 1000)
+
+        if miliseconds_passed > settings.EMAIL_VALIDATE_MESSAGE_AGE:
+            self.__context["fail_message"] = "this link is expired"
+            self.__context.update(self.extra_context)
+
+            return render_to_string(self.confirm_failed_template, self.__context)
+        elif not settings.VALIDATION_EMAILS_SENT.get(user.username, "") == token:
+            self.__context["fail_message"] = "this link is not the same that was sent to you or is some old link"
+            self.__context.update(self.extra_context)
+
+            return render_to_string(self.confirm_failed_template, self.__context)
+        elif user.email_validated:
+            self.__context["fail_message"] = "your email is already validated"
+            self.__context.update(self.extra_context)
+
+            return render_to_string(self.confirm_failed_template, self.__context)
+        else:
+            self.__context["success_message"] = "your email is now validated"
+            self.__context.update(self.extra_context)
+
+            user.email_validated = True
+            perm = is_permission_created_create(
+                "valid_email", "O usuário tem um email validado", 
+                Dummy
+            )
+            user.user_permissions.add(perm)
+
+            return render_to_string(self.confirm_success_template, self.__context)
+
--- authsys/urls.py
+++ authsys/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.Profile.as_view(), name="profile"),
     path('change/done/', views.ChangeDone.as_view(), name="profile_changes_done"),
+    path('confirm/email/', views.ConfirmEmail.as_view(), name="confirm_email"),
     path('login/', views.LogIn.as_view(), name="login"),
     path('logout/', views.LogOut.as_view(), name="logout"),
     path('signup/', views.SignUp.as_view(), name="signup"),
