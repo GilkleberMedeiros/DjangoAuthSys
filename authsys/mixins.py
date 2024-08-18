ff4af824331f7826a77ec05140db5cdbc490d0fe
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-08-18 14:01:12 -0300

    'ConfirmValidationEmailMixin.valid_email' retorna um HttpResponse.

--- authsys/mixins.py
+++ authsys/mixins.py
@@ -51,9 +51,9 @@ class ConfirmValidationEmailMixin:
     __context = {}
     extra_context = {}
 
-    def valid_email(self, user, token, datetime):
+    def valid_email(self, request, user, token, datetime):
         from datetime import datetime as dt
-        from django.template.loader import render_to_string
+        from django.shortcuts import render
         from .utils import is_permission_created_create
         from .models import Dummy
 
@@ -63,17 +63,17 @@ class ConfirmValidationEmailMixin:
             self.__context["fail_message"] = "this link is expired"
             self.__context.update(self.extra_context)
 
-            return render_to_string(self.confirm_failed_template, self.__context)
+            return render(request, self.confirm_failed_template, self.__context)
         elif not settings.VALIDATION_EMAILS_SENT.get(user.username, "") == token:
             self.__context["fail_message"] = "this link is not the same that was sent to you or is some old link"
             self.__context.update(self.extra_context)
 
-            return render_to_string(self.confirm_failed_template, self.__context)
+            return render(request, self.confirm_failed_template, self.__context)
         elif user.email_validated:
             self.__context["fail_message"] = "your email is already validated"
             self.__context.update(self.extra_context)
 
-            return render_to_string(self.confirm_failed_template, self.__context)
+            return render(request, self.confirm_failed_template, self.__context)
         else:
             self.__context["success_message"] = "your email is now validated"
             self.__context.update(self.extra_context)
@@ -85,5 +85,5 @@ class ConfirmValidationEmailMixin:
             )
             user.user_permissions.add(perm)
 
-            return render_to_string(self.confirm_success_template, self.__context)
+            return render(request, self.confirm_success_template, self.__context)
 
